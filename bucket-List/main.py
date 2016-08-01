74e096f5f5cb9719b1754fe680bd028049cd1a73
Sisco22 <cssi2016@CSSIs-MacBook-Air.local>
2016-08-01 13:24:01 -0500

    Working progress page

--- bucket-List/main.py
+++ bucket-List/main.py
@@ -1,8 +1,12 @@
 
 # Hello
 
-import webapp2
 import jinja2
+import urllib
+
+from google.appengine.ext import ndb
+
+import webapp2
 
 env= jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
@@ -13,26 +17,19 @@ class results(ndb.Model):
 
 class BucketListHandler(webapp2.RequestHandler):
     def get(self):
-<<<<<<< Updated upstream
     	bucketlistproto_template= env.get_template('bucket_list_form.html')
-    	'bucketListItem': self.request.get("bucketListItem")
     	self.response.out.write(bucketlistproto_template.render())
     def post(self):
-    	form-results_template= env.get_template('form_results.html')
+    	form_results_template= env.get_template('form_results.html')
     	variables = {
     		'bucketListItem':self.response.get('bucketListItem'),
     	}
-=======
-        bucketlistproto_template= env.get_template('bucket-list-form.html')
+        bucketlistproto_template= env.get_template('bucket_list_form.html')
         self.response.out.write(bucketlistproto_template.render())
-    def post(self):
-        form-results_template= env.get_template('form-results.html')
-        variables = {
-        'bucketListItem':self.response.get('bucketListItem'),
-        }
->>>>>>> Stashed changes
-    self.repsonse.out.write(form-results_template.render(variables))
+
+#class results
 
 app = webapp2.WSGIApplication([
-    ('/', BucketListHandler)
+    ('/', BucketListHandler),
+#    ('/'results, ResultsLishandler)
 ], debug=True)
