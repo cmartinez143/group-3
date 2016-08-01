5b6f682ffa172dee885a3a7b9587328a99c3bc78
Sisco22 <cssi2016@CSSIs-MacBook-Air.local>
2016-08-01 13:12:03 -0500

    k

--- bucket-List/main.py
+++ bucket-List/main.py
@@ -7,12 +7,13 @@ import jinja2
 env= jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 class results(ndb.Model):
-	event = ndb.StringProperty(indexed=False)
-	location = ndb.StringProperty(indexed=False)
+    event = ndb.StringProperty(indexed=False)
+    location = ndb.StringProperty(indexed=False)
 
 
 class BucketListHandler(webapp2.RequestHandler):
     def get(self):
+<<<<<<< Updated upstream
     	bucketlistproto_template= env.get_template('bucket_list_form.html')
     	'bucketListItem': self.request.get("bucketListItem")
     	self.response.out.write(bucketlistproto_template.render())
@@ -21,8 +22,17 @@ class BucketListHandler(webapp2.RequestHandler):
     	variables = {
     		'bucketListItem':self.response.get('bucketListItem'),
     	}
+=======
+        bucketlistproto_template= env.get_template('bucket-list-form.html')
+        self.response.out.write(bucketlistproto_template.render())
+    def post(self):
+        form-results_template= env.get_template('form-results.html')
+        variables = {
+        'bucketListItem':self.response.get('bucketListItem'),
+        }
+>>>>>>> Stashed changes
     self.repsonse.out.write(form-results_template.render(variables))
 
 app = webapp2.WSGIApplication([
-    ('/bucket', BucketListHandler)
+    ('/', BucketListHandler)
 ], debug=True)
