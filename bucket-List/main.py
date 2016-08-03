f07cde36df8b13366b69b7e2fec032dce610a1c0
DantePulley <pulleydante@gmail.com>
2016-08-03 16:21:34 -0500

    Fixes Here

--- bucket-List/main.py
+++ bucket-List/main.py
@@ -73,7 +73,7 @@ class MainHandler(webapp2.RequestHandler):
                 self.redirect("/bucketlistform")
             else:
                 # no user data; redirect to regitration
-                self.redirect("/register")
+                self.redirect("/home")
         else:
             self.redirect("/home")
 class HomeHandler(webapp2.RequestHandler):
