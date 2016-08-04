9ae8d49968d57d9942ae4dd7afa0ad0199a47207
DantePulley <pulleydante@gmail.com>
2016-08-04 09:53:51 -0500

    Changes

--- bucket-List/main.py
+++ bucket-List/main.py
@@ -71,8 +71,19 @@ class MainHandler(webapp2.RequestHandler):
                 # found user data; redirect to bucketform
                 self.redirect("/bucketlistform")
             else:
+<<<<<<< Updated upstream
                 # no user data; redirect to regitration
                 self.redirect("/register")
+=======
+
+                # no user data; redirect to regitration
+                self.redirect("/register")
+
+
+                # no user data; redirect to regitration
+                self.redirect("/register")
+
+>>>>>>> Stashed changes
                 # no user data; redirect to registration
                 self.redirect("/home")
         else:
